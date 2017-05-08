2b5532e14df96b38cdf05461c830dd34d7cb21d1
Shubhankar <shubhankar1100@gmail.com>
2017-05-09 00:54:17 +0530

    Update logic.js

--- TechIota/javaScript/logic.js
+++ TechIota/javaScript/logic.js
@@ -1,7 +1,33 @@
 /**
- * 
+ * open the url
  */
 function openInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
 }
+
+/**
+ * send the mail to email address
+ */
+$(document).ready(function() {
+    $('#send').click(function() {
+        $('#form1').attr('action',
+                       'mailto:shubhankarstar@gmail.com?subject=' +
+                       $('#name').val() + '&body=' + $('#email').val() + $('#subject').val() + $('#txtArea').val());
+        $('#form1').submit();
+    });
+});
+
+/**
+ * About card to have some 3d effect while mouse hover
+ */
+$(document).ready(function() {
+	  $(".w3-card").hover(function(){ $(this).toggleClass('aboutCard'); });
+	});
+
+/**
+ * Interview Question card to have some 3d effect while mouse hover
+ */
+$(document).ready(function() {
+	  $(".interviewQuestion").hover(function(){ $(this).toggleClass('questionCard'); });
+	});
